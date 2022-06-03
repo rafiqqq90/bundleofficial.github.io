dbdebeee8824cd24c4c7347df995ebc22a9ae05b
rafiqqq90 <muhdrafiqk90@gmail.com>
2022-06-03 10:43:27 +0800

    bundle

---
+++ bundle/admin/partials/footer.php
@@ -0,0 +1,12 @@
+<!-- Footer Section Starts -->
+
+<div class="footer">
+    <div class="wrapper">
+        <p class="text-center">2022 All rights reserved, Bundle Shop</p>
+    </div>
+</div>
+<!-- Footer Section Ends -->
+
+</body>
+
+</html>
